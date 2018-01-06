2f4e3245284a955d9aee5f5f84b0677734f1dc40
Andrew Loomis <andrew@thanics.com>
2018-01-06 12:50:55 -0500

    I2C now working on BatteryControl ATTiny44

--- Software/Drone/ATTiny44_BatteryController/BatteryControl/BatteryControl.ino
+++ Software/Drone/ATTiny44_BatteryController/BatteryControl/BatteryControl.ino
@@ -1,21 +1,99 @@
-//#include <USIWire.h>
-//#include "TinyWire.h"
-#include <SoftwareSerial.h>
-SoftwareSerial mySerial(1,10);
-//#define addr 0x03
-//#define pwrControl 10
+#include "TinyWire.h"
+#define addr 0x04
+
+#define pwrControl 10
 #define tempPin A7
+#define sensePin 9
+#define cellPin1 A0
+#define cellPin2 A1
+#define cellPin3 A2
+#define cellPin4 A3
+
+//Analog ADC vales
+unsigned long aCellValue1 = 0;
+unsigned long aCellValue2 = 0;
+unsigned long aCellValue3 = 0;
+unsigned long aCellValue4 = 0;
+
+//Actual cell voltages
+float Cell1 = 0.000;  
+float Cell2 = 0.000;
+float Cell3 = 0.000;
+float Cell4 = 0.000;
+
+float adcVoltage = 0.0048; //one point on the ADC equals this many volts
+
+float temp = 0;
 
 void setup() {
-//  pinMode(pwrControl, OUTPUT);
-//  digitalWrite(pwrControl, HIGH);
-//  TinyWire.begin(addr);
-  mySerial.begin(9600);
+  //Latch power
+  pinMode(pwrControl, OUTPUT);
+  digitalWrite(pwrControl, HIGH);
+
+  //Initialize comms
+  TinyWire.begin(addr);
+  TinyWire.onRequest(onI2CRrequest);
+  //mySerial.begin(9600);
 }
 
 void loop() {
-  int tempOutput = analogRead(tempPin);
-  float temp = ((tempOutput * 0.0048828125) - 0.5) / 0.01;                      //Temp sensor's Vout = Tc x Ta + V0, Tc = 10mV/C, V0 = 500mV
-  mySerial.println(temp);
-  delay(1000);
+  //If button pressed
+  if (digitalRead(sensePin) == HIGH) {
+    handleInterrupt();
+  }
+  getTemp();
+  //getCellVoltages();
 }
+
+void onI2CRrequest() {
+  TinyWire.send(temp);
+}
+
+void getTemp() {
+  //Temperature Reading
+  int tempOutput = analogRead(tempPin);
+  temp = ((tempOutput * adcVoltage) - 0.5) / 0.01;    //Temp sensor's Vout = Tc x Ta + V0, Tc = 10mV/C, V0 = 500mV
+}
+
+//void getCellVoltages() {
+//  //reading cell voltages in analog pins
+//  int iterations = 2000;
+//  
+//  for(int i=0;i < iterations;i++){
+//    aCellValue1 = aCellValue1 + analogRead(cellPin1);
+//    aCellValue2 = aCellValue2 + analogRead(cellPin2);
+//    aCellValue3 = aCellValue3 + analogRead(cellPin3);
+//    aCellValue4 = aCellValue4 + analogRead(cellPin4);
+//  }
+//   
+//  aCellValue1 = aCellValue1 / iterations;
+//  aCellValue2 = aCellValue2 / iterations;
+//  aCellValue3 = aCellValue3 / iterations;
+//  aCellValue4 = aCellValue4 / iterations;
+//
+//  //convert analog values to voltages
+//  Cell1 = (aCellValue1 * adcVoltage);
+//  Cell2 = (aCellValue2 * adcVoltage * 2) - Cell1;
+//  Cell3 = (aCellValue3 * adcVoltage * 3) - Cell2 - Cell1;
+//  Cell4 = (aCellValue4 * adcVoltage * 4) - Cell3 - Cell2 - Cell1;
+//
+//}
+
+void handleInterrupt() {
+  //Wait for 1 sec button hold
+  delay(1000);
+  if (digitalRead(sensePin) == HIGH) {
+    //Turn off power MOSFET, shut down
+    digitalWrite(pwrControl, LOW);
+  }
+}
+
+
+
+
+
+
+
+
+
+
