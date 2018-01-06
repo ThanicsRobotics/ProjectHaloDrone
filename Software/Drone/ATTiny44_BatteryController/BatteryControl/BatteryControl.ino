d8512235fd9ec9c7f2a3b65cfca3927ecc45c347
Andrew Loomis <andrew@thanics.com>
2018-01-05 23:01:15 -0500

    Moved TinyWire files to local library directory, BatteryControl now can read temperature

--- Software/Drone/ATTiny44_BatteryController/BatteryControl/BatteryControl.ino
+++ Software/Drone/ATTiny44_BatteryController/BatteryControl/BatteryControl.ino
@@ -1,11 +1,21 @@
-#include <TinyWire.h>
+//#include <USIWire.h>
+//#include "TinyWire.h"
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(1,10);
+//#define addr 0x03
+//#define pwrControl 10
+#define tempPin A7
 
 void setup() {
-  // put your setup code here, to run once:
-zSDfsdfadbxcbxcvb
+//  pinMode(pwrControl, OUTPUT);
+//  digitalWrite(pwrControl, HIGH);
+//  TinyWire.begin(addr);
+  mySerial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int tempOutput = analogRead(tempPin);
+  float temp = ((tempOutput * 0.0048828125) - 0.5) / 0.01;                      //Temp sensor's Vout = Tc x Ta + V0, Tc = 10mV/C, V0 = 500mV
+  mySerial.println(temp);
+  delay(1000);
 }
