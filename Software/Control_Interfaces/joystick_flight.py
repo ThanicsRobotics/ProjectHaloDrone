7da156df60eabee31a975b309628f3480654d5b2
Braidan <legohead259@gmail.com>
2018-01-12 12:47:21 -0500

    Fixed basic flight capability to actually work properly

--- Software/Control_Interfaces/joystick_flight.py
+++ Software/Control_Interfaces/joystick_flight.py
@@ -1,7 +1,8 @@
 from adc_interface import get_pwm
 from radio_interface import send
 
-motor_vals = get_pwm()[0, 3]
-print motor_vals  # Debug
-send(motor_vals)
+while True:
+    motor_vals = get_pwm()[0, 3]
+    print motor_vals  # Debug
+    send(motor_vals)
 
