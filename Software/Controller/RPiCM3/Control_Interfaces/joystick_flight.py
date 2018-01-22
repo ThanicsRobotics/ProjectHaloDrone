a8dad603fcf35d46f2aafae9f4ecefce38e9682b
Andrew Loomis <andrew@thanics.com>
2018-01-22 17:53:47 -0500

    get center values

--- Software/Controller/RPiCM3/Control_Interfaces/joystick_flight.py
+++ Software/Controller/RPiCM3/Control_Interfaces/joystick_flight.py
@@ -8,20 +8,24 @@ def calibrateJoystickRange():
     time.sleep(3)
     
     adc_interface.yawCenter = motor_vals[0]
+    print(adc_interface.yawCenter)
     adc_interface.altitudeCenter = motor_vals[1]
+    print(adc_interface.altitudeCenter)
     adc_interface.rollCenter = motor_vals[2]
+    print(adc_interface.rollCenter)
     adc_interface.pitchCenter = motor_vals[3]
+    print(adc_interface.pitchCenter)
 
 calibrateJoystickRange()
 while True:
     adc_interface.get_calibrated_pwm()
     #print motor_vals  # Debug
-    send(3)
-    send(adc_interface.calibrated_pwm[1])
-    send(4)
-    send(adc_interface.calibrated_pwm[2])
-    send(5)
-    send(adc_interface.calibrated_pwm[3])
-    send(6)
-    send(adc_interface.calibrated_pwm[0])
-    time.sleep(.5)
+    # send(3)
+    # send(adc_interface.calibrated_pwm[1])
+    # send(4)
+    # send(adc_interface.calibrated_pwm[2])
+    # send(5)
+    # send(adc_interface.calibrated_pwm[3])
+    # send(6)
+    # send(adc_interface.calibrated_pwm[0])
+    # time.sleep(.5)
