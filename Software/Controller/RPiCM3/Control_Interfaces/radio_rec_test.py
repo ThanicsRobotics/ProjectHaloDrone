b86c40afa3cda77c79b3f6fa2037805ea7a3a980
andrewloomis <andrew@thanics.com>
2018-01-31 16:26:32 +0000

    added python keyboard interrupt handling

--- Software/Controller/RPiCM3/Control_Interfaces/radio_rec_test.py
+++ Software/Controller/RPiCM3/Control_Interfaces/radio_rec_test.py
@@ -4,6 +4,10 @@ port = serial.Serial(port="/dev/ttyAMA0", baudrate=9600, timeout=1)
 ack = 0
 connected = True
 
-while connected:
-    bit_rec = port.read()
-    print "Recieved:", bit_rec
+try:
+    while connected:
+        bit_rec = port.read()
+        print "Recieved:", bit_rec
+
+except KeyboardInterrupt:
+    port.close()
