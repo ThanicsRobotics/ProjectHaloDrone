ab9fce604b77c086bc710eda3b62a0567e84ae0f
andrewloomis <andrew@thanics.com>
2018-02-03 15:28:00 +0000

    Cleaned up STM32 updating code

--- Software/Drone/RPiCM3/updateSTM32Firmware.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo rm /home/pi/STM32Programs/firmware.elf
-sudo cp /home/pi/ProjectHalo/Software/Drone/STM32F446/.pioenvs/nucleo_f446re/firmware.elf /home/pi/STM32Programs/
-cd /home/pi/STM32Programs/
-sudo openocd
