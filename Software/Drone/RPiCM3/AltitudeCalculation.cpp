64779fecf1cf430f0453116ccdd6d22485dd7df7
Andrew Loomis <andrew@thanics.com>
2018-02-04 21:14:15 -0500

    changed delay

--- Software/Drone/RPiCM3/AltitudeCalculation.cpp
+++ Software/Drone/RPiCM3/AltitudeCalculation.cpp
@@ -307,7 +307,7 @@ int getUltrasonicData(int sensor, int iterations) {
 
         //Starting TRIG pulse
         digitalIOWrite(pin, HIGH);
-        delayMicroseconds(20);
+        delayMicroseconds(30);
         digitalIOWrite(pin, LOW);
 
         //Wait until pulse is complete (when handleEcho is complete)
