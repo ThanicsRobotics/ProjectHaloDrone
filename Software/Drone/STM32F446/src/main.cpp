45e49a681e35fd6428297ae0cc89a399614d239a
Andrew Loomis <andrew@thanics.com>
2018-02-08 13:41:38 -0500

    added delay to serial

--- Software/Drone/STM32F446/src/main.cpp
+++ Software/Drone/STM32F446/src/main.cpp
@@ -519,7 +519,7 @@ int main() {
     gyro_signalen();
 
     //Load gyro angle data into SPI buffer
-    spi.reply((signed char)-15 << 8 | (signed char)31);
+    spi.reply((signed char)angle_pitch << 8 | (signed char)angle_roll);
     
     //spi.reply((int)receiver_input_pitch);
     //CLOCK SPEED TEST
