f86e272afbb2b482c331c79e9b52fdd78a736533
Andrew Loomis <andrew@thanics.com>
2018-02-07 19:37:01 -0500

    removed debug

--- Software/Drone/RPiCM3/AltitudeCalculation.cpp
+++ Software/Drone/RPiCM3/AltitudeCalculation.cpp
@@ -206,8 +206,8 @@ void handleSerialInterrupt() {
             throttleInput = data;                                            //Set throttle input
             pthread_mutex_unlock(&serial_mutex);
             coFlag = false;
-            cout << throttleInput << endl;
-            fflush(stdout);
+            //cout << throttleInput << endl;
+            //fflush(stdout);
         }
         //cout << "3" << endl;
         else if (data == 3) coFlag = true;                                  //If data is 3 (throttle coefficient), flag the value
