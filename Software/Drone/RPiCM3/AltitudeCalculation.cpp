5766bd8b48e446e7531e9aa26f9bd2f2e1e6cf04
Andrew Loomis <andrew@thanics.com>
2018-02-10 18:17:52 -0500

    testing throttle transmission

--- Software/Drone/RPiCM3/AltitudeCalculation.cpp
+++ Software/Drone/RPiCM3/AltitudeCalculation.cpp
@@ -88,8 +88,9 @@ void getGyroValues() {
     //Gyro pitch and roll are stored in two incoming bytes
     //wiringPiSPIDataRW(SPI_CS, gyroBuffer, 2);
     spiXfer(spiFd, gyroBuffer, gyroBuffer, 2);
-    gyroPitch = (signed char)gyroBuffer[0];
-    gyroRoll = (signed char)gyroBuffer[1];
+    //gyroPitch = (signed char)gyroBuffer[0];
+    //gyroRoll = (signed char)gyroBuffer[1];
+    cout << (int)(gyroBuffer[0] << 8 | gyroBuffer[1]) << endl;
 }
 
 void setupSPI() {
@@ -253,7 +254,7 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
     signal(SIGINT, signal_callback_handler);
-    
+
     setupIOExpander();
 
     pthread_create(&serialThread, NULL, serialLoop, NULL);
