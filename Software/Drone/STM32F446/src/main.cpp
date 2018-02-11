2dcf68d3e819473f86421e77339f138ea128807d
Andrew Loomis <andrew@thanics.com>
2018-02-11 12:37:18 -0500

    fixed syntax

--- Software/Drone/STM32F446/src/main.cpp
+++ Software/Drone/STM32F446/src/main.cpp
@@ -381,9 +381,9 @@ int main() {
 //       //do stuff thats not flight
       
 //       //Load gyro angle data into SPI buffer
-      //spi.reply((signed char)gyro_roll_input << 8 | (signed char)gyro_pitch_input);
+      spi.reply((signed char)gyro_roll_input << 8 | (signed char)gyro_pitch_input);
 //       //spi.reply((int)receiver_input_throttle);
-      spi.reply(whoami);
+      //spi.reply(whoami);
     }
     loop_timer = onTime.read_us();
     //gyro_signalen();
