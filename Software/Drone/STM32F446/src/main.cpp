8631302527eb95caebeed79f8da37c96610d0fc8
Andrew Loomis <andrew@thanics.com>
2018-02-21 18:56:57 -0500

    trimmed loop time

--- Software/Drone/STM32F446/src/main.cpp
+++ Software/Drone/STM32F446/src/main.cpp
@@ -421,28 +421,28 @@ int main() {
   //Infinite PID Loop
   while(1) {
     //For starting the motors: throttle low and yaw left (step 1)
-    // if((receiver_input_throttle < 1050 && receiver_input_yaw < 1050 && receiver_input_yaw > 990) || armed) {
-    //   start = 1;
-    //   //pc.printf("Armed\r\n");
-    // }
+    if((receiver_input_throttle < 1050 && receiver_input_yaw < 1050 && receiver_input_yaw > 990) || armed) {
+      start = 1;
+      //pc.printf("Armed\r\n");
+    }
     
-    // //When yaw stick is back in the center position start the motors (step 2)
-    // if ((start == 1 && receiver_input_throttle < 1050 && receiver_input_yaw > 1450) || armed) {
-    //   start = 2;
-    //   angle_pitch = angle_pitch_acc;                                         //Set the gyro pitch angle equal to the accelerometer pitch angle when the quadcopter is started.
-    //   angle_roll = angle_roll_acc;                                           //Set the gyro roll angle equal to the accelerometer roll angle when the quadcopter is started.
-    //   gyro_angles_set = true;                                                //Set the IMU started flag.
+    //When yaw stick is back in the center position start the motors (step 2)
+    if ((start == 1 && receiver_input_throttle < 1050 && receiver_input_yaw > 1450) || armed) {
+      start = 2;
+      angle_pitch = angle_pitch_acc;                                         //Set the gyro pitch angle equal to the accelerometer pitch angle when the quadcopter is started.
+      angle_roll = angle_roll_acc;                                           //Set the gyro roll angle equal to the accelerometer roll angle when the quadcopter is started.
+      gyro_angles_set = true;                                                //Set the IMU started flag.
       
-    //   //Reset the PID controllers for a bumpless start.
-    //   pid_i_mem_roll = 0;
-    //   pid_last_roll_d_error = 0;
-    //   pid_i_mem_pitch = 0;
-    //   pid_last_pitch_d_error = 0;
-    //   pid_i_mem_yaw = 0;
-    //   pid_last_yaw_d_error = 0;
+      //Reset the PID controllers for a bumpless start.
+      pid_i_mem_roll = 0;
+      pid_last_roll_d_error = 0;
+      pid_i_mem_pitch = 0;
+      pid_last_pitch_d_error = 0;
+      pid_i_mem_yaw = 0;
+      pid_last_yaw_d_error = 0;
 
-    //   armed = false;
-    // }
+      armed = false;
+    }
 
     // //Stopping the motors: throttle low and yaw right.
     // if (start == 2 && receiver_input_throttle < 1050 && receiver_input_yaw > 1950) {
@@ -535,7 +535,7 @@ int main() {
     //   spi.reply((signed char)angle_pitch << 8 | (signed char)angle_roll);
     //   //loopCount += 1;
     // }
-    if ((onTime.read_us() - loop_timer < 4000)) {
+    if ((onTime.read_us() - loop_timer < 4100)) {
       if (loopCount % 2 == 0) {
         spi.reply(((signed char)angle_pitch << 8) | ((signed char)angle_roll & 0xFF));
       }
