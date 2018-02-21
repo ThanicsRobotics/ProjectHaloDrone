3f789ab02ad543897ce595494e0bb3a1af94b8c8
Andrew Loomis <andrew@thanics.com>
2018-02-20 20:46:43 -0500

    added declaration

--- Software/Drone/RPiCM3/src/spi.h
+++ Software/Drone/RPiCM3/src/spi.h
@@ -9,6 +9,7 @@ extern char stm32_rx_buffer[100];
 extern char stm32_tx_buffer[100];
 extern bool spiConfigured;
 extern bool authenticated;
+extern bool preStart;
 
 //CS0 is barometer, CS1 is STM32 flight controller
 extern int SPI_CS;
