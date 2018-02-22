e31cbce3e1cb458061099f372098406ab54d6d67
andrewloomis <andrew@thanics.com>
2018-02-22 18:40:57 -0500

    changed pwr management reg to 0

--- Software/Drone/STM32F446/src/main.cpp
+++ Software/Drone/STM32F446/src/main.cpp
@@ -295,7 +295,7 @@ void set_gyro_registers(){
   memset(cmd,0,sizeof(cmd));
 
   cmd[0] = 0x06;                                                                //We want to write to the PWR_MGMT_1 register (06 hex)
-  cmd[1] = 0x01;                                                                //Set the register bits as 00000000 to activate the gyro
+  cmd[1] = 0x00;                                                                //Set the register bits as 00000000 to activate the gyro
   i2c.write(gyro_address, cmd, 2);
   memset(cmd,0,sizeof(cmd));
 
