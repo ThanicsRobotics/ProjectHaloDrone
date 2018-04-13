dbd6cb3c330c5231902a5ee70677bd5581a580bc
andrewloomis <andrew@thanics.com>
2018-04-13 22:57:23 +0000

    updated atmega updater code

--- RPiCM3/update/updater.cpp
+++ RPiCM3/update/updater.cpp
@@ -8,57 +8,6 @@
 
 using namespace std;
 
-int main() {
-    cout << "**Make sure you have put all firmwares being updated in the firmwares/ folder**" << endl << "Type 'ok' to continue: ";
-    string input = "";
-    getline(cin, input);
-    if (input != "ok") exit(1);
-
-    wiringPiSetupGpio();
-    pinMode(SEL0, OUTPUT);
-    pinMode(SEL1, OUTPUT);
-    pinMode(SEL2, OUTPUT);
-
-    cout << "Which processor to update?\n" << "\t1 = STM32F446 (Flight Motor Controller)\n" << "\t2 = STM32F103 (Gimbal Controller)\n"
-        << "\t3 = ATMEGA8 (ESC1)\n" << "\t4 = ATMEGA8 (ESC2)\n" << "\t5 = ATMEGA8 (ESC3)\n" << "\t6 = ATMEGA8 (ESC4)\n" << "\t7 = ALL ESCs\n"
-        << "\t8 = ALL Processors\n" << "Choice: ";
-    
-    input = "";
-    getline(cin, input);
-
-    switch (input) {
-        case "1":
-            updateSTM32F446();
-            break;
-        case "2":
-            updateSTM32F103();
-            break;
-        case "3":
-            updateESC1();
-            break;
-        case "4":
-            updateESC2();
-            break;
-        case "5":
-            updateESC3();
-            break;
-        case "6":
-            updateESC4();
-            break;
-        case "7":
-            updateAllESCs();
-            break;
-        case "8":
-            updateSTM32F446();
-            updateSTM32F103();
-            updateAllESCs();
-            break;
-        default:
-            cout << "Invalid Response. Shutting Down." << endl;
-            exit(0);
-    }
-}
-
 void updateSTM32F446() {
     cout << "Updating STM32F446..." << endl;
     digitalWrite(SEL2, LOW);
@@ -73,16 +22,27 @@ void updateSTM32F103() {
 
 void updateESC1() {
     cout << "Updating ESC1..." << endl;
-    digitalWrite(SEL0, LOW);
+    digitalWrite(SEL0, HIGH);
     digitalWrite(SEL1, LOW);
-    system("avrdude -p m8 -C avrdude.conf -c cm3 -v");
+    system("avrdude -p m8 -C avrdude.conf -c pi_1 -v");
 }
 
 void updateESC2() {
-    cout << "Updating ESC2..." << endl;
-    digitalWrite(SEL0, HIGH);
+    cout << "Would you like to program(p) or set fuses(f)? (p/f): ";
+    digitalWrite(SEL0, LOW);
     digitalWrite(SEL1, LOW);
-    system("avrdude -p m8 -C avrdude.conf -c cm3 -v");
+    
+    string input = "";
+    getline(cin, input);
+    if (input == "p") {
+	cout << "Programming..." << endl;
+	system("sudo avrdude -p m8 -C avrdude_esc.conf -c pi_1 -v -U flash:w:firmwares/simonkesc.hex:i");
+    }
+    if (input == "f") {
+	cout << "Setting fuses..." << endl;
+	system("sudo avrdude -p m8 -C avrdude_esc.conf -c pi_1 -v -U lfuse:w:0x2E:m -U hfuse:w:0xC9:m");
+    }
+
 }
 
 void updateESC3() {
@@ -104,4 +64,56 @@ void updateAllESCs() {
     updateESC2();
     updateESC3();
     updateESC4();
+}
+
+int main() {
+    cout << "**Make sure you have put all firmwares being updated in the firmwares/ folder**" << endl << "Type 'ok' to continue: ";
+    string input = "";
+    getline(cin, input);
+    if (input != "ok") exit(1);
+
+    wiringPiSetupGpio();
+    pinMode(SEL0, OUTPUT);
+    pinMode(SEL1, OUTPUT);
+    pinMode(SEL2, OUTPUT);
+
+    cout << "Which processor to update?\n" << "\t1 = STM32F446 (Flight Motor Controller)\n" << "\t2 = STM32F103 (Gimbal Controller)\n"
+        << "\t3 = ATMEGA8 (ESC1)\n" << "\t4 = ATMEGA8 (ESC2)\n" << "\t5 = ATMEGA8 (ESC3)\n" << "\t6 = ATMEGA8 (ESC4)\n" << "\t7 = ALL ESCs\n"
+        << "\t8 = ALL Processors\n" << "Choice: ";
+    
+    input = "";
+    getline(cin, input);
+    int intInput = atoi(input.c_str());
+
+    switch (intInput) {
+        case 1:
+            updateSTM32F446();
+            break;
+        case 2:
+            updateSTM32F103();
+            break;
+        case 3:
+            updateESC1();
+            break;
+        case 4:
+            updateESC2();
+            break;
+        case 5:
+            updateESC3();
+            break;
+        case 6:
+            updateESC4();
+            break;
+        case 7:
+            updateAllESCs();
+            break;
+        case 8:
+            updateSTM32F446();
+            updateSTM32F103();
+            updateAllESCs();
+            break;
+        default:
+            cout << "Invalid Response. Shutting Down." << endl;
+            exit(0);
+    }
 }
