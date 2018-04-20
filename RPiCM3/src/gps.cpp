86da903234bed773ddcb808633b7e4d42f0c8a52
Andrew Loomis <andrew@thanics.com>
2018-04-20 18:27:49 -0400

    Added NemaTode lib, credits page

--- RPiCM3/src/gps.cpp
+++ RPiCM3/src/gps.cpp
@@ -2,6 +2,8 @@
 #include <gps.h>
 //#include <altitude.h>
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
 
 #define GPS_ADDR 0x42
 
@@ -21,8 +23,15 @@ void readGPS() {
     // char i2cCommands[20] = {4,GPS_ADDR,2,7,1,0xFF,6,32,3,0};
     // char buffer[256];
     // bbI2CZip(2, i2cCommands, sizeof(i2cCommands), buffer, sizeof(buffer));
-    
-    char buffer[1000];
-    i2cReadI2CBlockData(gpsFd, 0xFF, buffer, 32);
-    cout << buffer << endl;
+    char nmeaMessage[82];
+    for (int i = 0; i < 82; i += 32) {
+        char buffer[32];
+        i2cReadI2CBlockData(gpsFd, 0xFF, buffer, 32);
+        for (int j = 0; j < 32; j++) {
+            nmeaMessage[j+i] = buffer[j];
+            if((i + j) > 80) break;
+        }
+    }
+    cout << nmeaMessage << endl;
+    memset(nmeaMessage, 0, sizeof(nmeaMessage));
 }
