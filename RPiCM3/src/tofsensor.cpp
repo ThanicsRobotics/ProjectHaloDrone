37a32e779ea8ae457f7923985ad10dbf7591a4d8
Christian Hardy <hardyc932@gmail.com>
2018-07-18 22:22:49 -0400

    Read and write to regsters

--- RPiCM3/src/tofsensor.cpp
+++ RPiCM3/src/tofsensor.cpp
@@ -6,9 +6,15 @@
 #include <errno.h>
 #include <wiringPi.h>
 
+#define I2C_CONT_RW         0x00     // R/W
+#define I2C_CONT_RW_DATA    0xC00040 // R/W  Binary: 010000000000000001000000
+#define PHASE_OUT           0x08     // R
+
+
 TOFSensor::TOFSensor()
 {
     setup();
+    ConfigureMonoshot();
     ReadPhaseOut();
     CalculateDistance();
     ReportDistance();
@@ -19,6 +25,15 @@ TOFSensor::~TOFSensor()
     i2cClose(i2cFd);
 }
 
+void TOFSensor::ConfigureMonoshot(){
+
+    i2cWriteByteData(i2cFd, I2C_CONT_RW, 0xC0); //Break 24 bit I2C_CONT_RW_DATA into 3 bytes for slave to recieve.
+    i2cWriteByteData(i2cFd, I2C_CONT_RW, 0x00);
+    i2cWriteByteData(i2cFd, I2C_CONT_RW, 0x40);
+    std:cout << "Configured" << std::endl;
+
+    }
+
 void TOFSensor::setup()
 {
     // Open I2C address
@@ -27,16 +42,13 @@ void TOFSensor::setup()
     }
     else i2cConfigured = true;
 
-    //Configurations
-    //NONE?
-
 
 }
 
 void TOFSensor::ReadPhaseOut(){
 
     char outputRegister[3];
-    i2cReadI2CBlockData(i2cFd, 0x08, outputRegister, 3);
+    i2cReadI2CBlockData(i2cFd, PHASE_OUT, outputRegister, 3);
     std::cout << outputRegister[0] << " " << outputRegister[1] << " " << outputRegister[2] << std::endl;
     phaseOut = (short)((outputRegister[1] << 8) |  (outputRegister[0]));
 }
@@ -51,6 +63,6 @@ void TOFSensor::ReportDistance(){
 while(1){
         int time = millis();                                                 
         std::cout << distance << std::endl;
-            while(millis() - time <= 4);
+            while(millis() - time <= 50);
 }
 }
